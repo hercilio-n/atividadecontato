571d4d8c3fe9ee1f4a2c30989dc3874064c4f7e2
hercilio-n <hercilio.neto2003@gmail.com>
2023-12-14 20:48:07 -0300

    Att ajuste cors

--- Aula/src/main/java/com/example/Aula/controller/FilmeController.java
+++ Aula/src/main/java/com/example/Aula/controller/FilmeController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,13 +22,15 @@ import com.example.Aula.repository.FilmeRepository;
 
 import jakarta.persistence.Column;
 
+
 @RestController
 @RequestMapping("/Filmes")
 public class FilmeController {
 	@Autowired
 	FilmeRepository repo;
 	
-	@GetMapping()
+	
+	@GetMapping("/consultar")
 	public ResponseEntity<List<Filme>> getFilmes() {
 		return ResponseEntity.status(HttpStatus.OK).body(repo.findAll());
 	}
