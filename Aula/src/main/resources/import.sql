bbed7c051e4a5df05c01ab88f5c3fdb463a235b7
hercilio-n <hercilio.neto2003@gmail.com>
2023-12-14 21:13:36 -0300

    teste

--- Aula/src/main/resources/import.sql
+++ Aula/src/main/resources/import.sql
@@ -1,1 +1,1 @@
-insert into tb_filmes(nome, anolan, diretor, sinopse, genero)values('star wars','2020','Obwan','filme legal','Acao');
+insert into tb_filmes(nome, anolan, diretor, sinopse, genero)values('str wars','2020','Obwan','filme legal','Acao');
